a20368276c8b916c19565cf3db6bb4e9209028a0
gautam365541 <gautam365541@gmail.com>
2024-11-21 10:39:34 +0530

    Add files via upload

---
+++ CarGame/CarGame.java
@@ -0,0 +1,24 @@
+// package ROAD;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+public class CarGame extends JFrame {
+    CarGame() {
+        setSize(900, 900);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        AppPanel panel = new AppPanel(600, 900);
+
+        panel.setBackground(new Color(0, 0, 0));
+        add(panel);
+
+    }
+
+    public static void main(String[] args) {
+        new CarGame().setVisible(true);
+    }
+}
